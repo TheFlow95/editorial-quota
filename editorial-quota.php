bc2c08e91a291d1641b9031ae7f1e72cd2d0ce47
TheFlow95 <florentin@airdeflorentin.home>
2014-08-20 22:45:41 +0200

    Lot of updates

--- editorial-quota.php
+++ editorial-quota.php
@@ -27,24 +27,68 @@ Class EditorialQuota
 {
 	public function __construct()
 	{
+		add_action('admin_init', array($this, 'register_settings'));
 		add_action('admin_menu', array($this, 'add_admin_menu'));
+
+		if ( get_option( 'eq_role' ) == false ) {
+			update_option( 'eq_role', 'author' );
+		}
+		if ( get_option( 'eq_quota' ) == false ) {
+			update_option( 'eq_quota', '10' );
+		}
 	}
 
-	/*public static function install()
+	public function register_settings()
 	{
-		global $wpdb;
+	    register_setting('eq_settings', 'eq_role');
+		register_setting('eq_settings', 'eq_quota');
+		add_settings_section('eq_section', '', array($this, 'section_form'), 'eq_settings');
+		add_settings_field('eq_role', 'Authors\' role', array($this, 'role_form'), 'eq_settings', 'eq_section');
+		add_settings_field('eq_quota', 'Authors\' quota', array($this, 'quota_form'), 'eq_settings', 'eq_section');
+	}
 
-		$wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}editorial_quota (id INT PRIMARY KEY, quota INT NOT NULL);");
-	}*/
+	public function section_form()
+	{
+		echo '<p>Select the role to which to apply the quota and the posts\' quota to perform by month</p>';
+	}
+	
+	public function role_form()
+	{
+	    echo '<select>';
+	    wp_dropdown_roles(get_option('eq_role'));
+	    echo '</select>';
+	}
+
+	public function quota_form()
+	{
+		echo '<input id="eq_quota" type="number" min="0" class="regular-text" name="eq_quota"  value="'.get_option('eq_quota').'">';
+	}
 
 	public function add_admin_menu()
 	{
-		add_options_page( 'Réglages de Editorial Quota', 'Editorial Quota', 'manage_options', 'editorial-quota', array($this, 'menu_page'));
+		add_options_page( 'Editorial Quota Options', 'Editorial Quota', 'manage_options', 'editorial-quota', array($this, 'menu_html') );
+		add_management_page( 'Quota', 'Quota', 'publish_posts', 'quota', array($this, 'quota_html') );
 	}
 
 	public function menu_html()
 	{
-		echo '<h1>'.get_admin_page_title().'</h1>';
-		echo '<p>Bienvenue sur la page d\'accueil du plugin</p>';
+		echo '<div class="wrap">';
+		echo '<h2>'.get_admin_page_title().'</h2>';
+		echo '<form method="post" action="options.php">';
+		settings_fields('eq_settings');
+		do_settings_sections('eq_settings');
+		submit_button();
+		echo '</form>';
+		echo '</div>';
+	}
+	
+	public function quota_html()
+	{
+		echo '<div class="wrap">';
+		echo '<h2>'.get_admin_page_title().'</h2>';
+		echo '<p>Welcome to the homepage of Editorial Quota</p>';
+		echo '</div>';
 	}
 }
+
+new EditorialQuota();
